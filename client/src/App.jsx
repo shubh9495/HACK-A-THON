8150aa3fe8b89fb26d95bc21a9e8dae7ced61b3a
serAnkii <recruitankitnegi2023@gmail.com>
2022-11-11 14:42:21 +0530

    feat:init

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/Header.jsx'
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+      {/* <Main />
+      <Footer /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../CSS/Header.css'
+const Header = () => {
+  return (
+    <>
+        <div className="header">
+        <h1>UTTARAKHAND</h1>
+        <h2>THE LAND OF GODS</h2>
+        </div>
+    </>
+  )
+}
+
+export default Header
