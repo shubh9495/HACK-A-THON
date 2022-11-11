f4c65ac2d1b2ba1dd491607ae3410e71121c704d
Shubham Shukla <shubhamshukla9594@gmail.com>
2022-11-11 12:03:56 +0000

    feat:changes to api

--- client/src/assets/Api.js
+++ client/src/assets/Api.js
@@ -1,13 +1,169 @@
 export const data = [
     {
         id:1,
-        name:"decentralised twitter",
-        description:"It is a decentralised clone of twitter",
-        image:"url('twitter.jpg')",
-        group:'blockchain',
-        gallery:'https://github.com/serAnkii/web3_twitter',
-        about:''
-    }      
+        name:"Kedarnath",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+        
+    {
+        id:2,
+        name:"Badrinath",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
 
+    {
+        id:3,
+        name:"Gangotri",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+    
+    {
+        id:4,
+        name:"Yamunotri",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:5,
+        name:"Dhari Devi",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:6,
+        name:"Tunganath",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:7,
+        name:"Kartik Swami",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:8,
+        name:"Triyuginarayan",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:9,
+        name:"Rudranath",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:10,
+        name:"Kalpeshwar",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:11,
+        name:"Chandrabadani",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:12,
+        name:"Chandrashila",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:13,
+        name:"Surkanda",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:14,
+        name:"Kalimath",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:15,
+        name:"Ponta Sahib",
+        image:"",
+        group:'temple',
+        link:''
+        
+    },
+
+    {
+        id:16,
+        name:"Ganga",
+        image:"",
+        group:'river',
+        link:''
+        
+    },
+
+    {
+        id:17,
+        name:"Yamuna",
+        image:"",
+        group:'river',
+        link:''
+        
+    }
+    
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 ]
